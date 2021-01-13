8b6edc73a0a07efd9af130096ad9e230dacb7a92
Gökhan Göksel Elpeze <gokselelpeze@gmail.com>
2021-01-13 21:58:09 +0300

    login.php

--- application/views/index.php
+++ application/views/index.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>DEUQUE HOMEPAGE</title>
-</head>
-<body>
+    <head>
+        <title>DEUQUE HOMEPAGE</title>
+    </head>
+    <body>
 
-<p>WELCOME TO DEUQUE</p>
-<p>Sa</p>
-<textarea name="message" rows="10" cols="30">
+        <p>WELCOME TO DEUQUE</p>
+        <p>Sa</p>
+        <textarea name="message" rows="10" cols="30">
 The cat was playing in the garden.
 </textarea>
-<a href="answers.php">Answers </a>
-</body>
+        <a href="answers">Answers </a>
+    </body>
 </html>
 
