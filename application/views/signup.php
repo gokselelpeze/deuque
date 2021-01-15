d5ffa645ad4e6451fc99773a85210435b520db97
Gökhan Göksel Elpeze <gokselelpeze@gmail.com>
2021-01-15 13:47:31 +0300

    profile page

--- application/views/signup.php
+++ application/views/signup.php
@@ -18,22 +18,29 @@
                     <div class="col-md-5 mx-auto">
                         <div class="card card-body">
 
-                            <form id="submitForm" action="/login" method="post" data-parsley-validate="" data-parsley-errors-messages-disabled="true" novalidate="" _lpchecked="1"><input type="hidden" name="_csrf" value="7635eb83-1f95-4b32-8788-abec2724a9a4">
+                            <form id="submitForm" action="login" method="post" data-parsley-validate=""
+                                  data-parsley-errors-messages-disabled="true" novalidate="" _lpchecked="1"><input
+                                        type="hidden" name="_csrf" value="7635eb83-1f95-4b32-8788-abec2724a9a4">
                                 <div class="form-group required">
-                                    <lSabel for="username">Username / Email</lSabel>
-                                    <input type="text" class="form-control text-lowercase" id="username" required="" name="username" value="">
+                                    <label for="username">Username</label>
+                                    <input type="text" class="form-control text-lowercase" id="username" required
+                                           name="username" value="">
                                 </div>
-                                <!-- Forget kısmı silinecek-->
+                                <div class="form-group required">
+                                    <label for="email">Email</label>
+                                    <input type="text" class="form-control text-lowercase" id="email" required
+                                           name="email" value="">
+                                </div>
+                                <div class="form-group required">
+                                    <label class="d-flex flex-row align-items-center" for="password">Password</label>
+                                    <input type="password" class="form-control" required id="password" name="password"
+                                           value="">
+                                </div>
                                 <div class="form-group required">
                                     <label class="d-flex flex-row align-items-center" for="password">Password
-                                        <a class="ml-auto border-link small-xl" href="/forget-password">Forget?</a></label>
-                                    <input type="password" class="form-control" required="" id="password" name="password" value="">
-                                </div>
-                                <!-- Forget kısmı silinecek-->
-                                <div class="form-group required">
-                                    <label class="d-flex flex-row align-items-center" for="password">Password Again
-                                        <a class="ml-auto border-link small-xl" href="/forget-password">Forget?</a></label>
-                                    <input type="password" class="form-control" required="" id="password" name="password" value="">
+                                        Again</label>
+                                    <input type="password" class="form-control" required id="passwordAgain"
+                                           name="passwordAgain" value="">
                                 </div>
                                 <div class="form-group pt-1">
                                     <button class="btn btn-primary btn-block" type="submit">Sign up</button>
