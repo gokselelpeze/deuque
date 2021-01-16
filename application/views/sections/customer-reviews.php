1b7d25c9c4131158ff47e86accf56cd026cf2644
bero <berhanay01@gmail.com>
2021-01-17 00:56:17 +0300

    Customer Reviews Slider

---
+++ application/views/sections/customer-reviews.php
@@ -0,0 +1,64 @@
+<hr />
+<br />
+<div class="container">
+    <div class="text-area text-center">
+        <h1>Customer Reviews</h1>
+    </div>
+
+    <div class='row'>
+        <div class='col-md-8 offset-lg-2'>
+
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner" role="listbox">
+
+                    <!-- Quote 1 -->
+                    <div class="carousel-item active">
+                        <blockquote>
+                            <div class="row">
+                                <div class="col-10 offset-1">
+                                    <p>"Was very pleased with the job Deschutes Window Cleaning did on our windows. Thank you! Will definitely recommend them to anyone needing this service."</p>
+                                    <small>Kim F.</small>
+                                </div>
+                            </div>
+                        </blockquote>
+                    </div>
+
+                    <div class="carousel-item">
+                        <blockquote>
+                            <div class="row">
+                                <div class="col-10 offset-1">
+                                    <p>"Our windows are crystal clear for the first time in a long time. Thank you, Kyle! Super friendly, efficient and proficient!"</p>
+                                    <small>Alice R.</small>
+                                </div>
+                            </div>
+                        </blockquote>
+                    </div>
+
+                    <div class="carousel-item">
+                        <blockquote>
+                            <div class="row">
+                                <div class="col-10 offset-1">
+                                    <p>"It went very well! Kyle was very nice and very professional. He was able to give me tips about my windows and even dusted off some of my hard to reach lighting fixtures. Very knowledgable, will definitely be hiring again!"
+                                    </p>
+                                    <small>Briana G.</small>
+                                </div>
+                            </div>
+                        </blockquote>
+                    </div> <!-- .item -->
+
+                </div> <!-- .carousel-inner -->
+            </div>  <!-- #my-carousel -->
+        </div>
+    </div>
+</div>
+
+
+<hr />
+
+<script>$('#myCarousel').carousel({
+        interval: 3000
+    });
+
+    window.sr = ScrollReveal({ reset: true });
+
+    sr.reveal('.company-stuff', { duration: 700 });</script>
