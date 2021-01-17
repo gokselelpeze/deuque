4a11c43405598fece0c88ac6f1d2bd7647722e2b
Gökhan Göksel Elpeze <gokselelpeze@gmail.com>
2021-01-18 00:24:22 +0300

    css

--- application/views/pages/fill.php
+++ application/views/pages/fill.php
@@ -7,12 +7,16 @@ $qnInfo = json_decode(json_encode($qn), true);
 $questionsInfo = json_decode(json_encode($questions), true);
 $userInfo = json_decode(json_encode($user), true);
 ?>
+<page size="A4">
+
 <div class="container mt-5 py-5">
     <h1 class="text-center"><?php echo $qnInfo[0]['questionnaire_name'] ?></h1>
     <h2 class="text-secondary text-center"><?php echo $qnInfo[0]['questionnaire_subtext'] ?></h2>
+
     <div class="ml-5 mt-3">
         <form>
             <?php foreach ($questionsInfo as $question) {
+                echo '<div class="question mb-5">';
                 echo '<fieldset id="' . $question['question_id'] . '">';
                 echo '<h2>' . $question['question_name'] . '</h2>';
                 echo '<h5 class="text-secondary">' . $question['question_subtext'] . '</h5>';
@@ -20,18 +24,21 @@ $userInfo = json_decode(json_encode($user), true);
                     if ($question['option_' . ($i + 1)] == null)
                         break;
                     echo '<div class="my-1">
-            <input class="form-check-input" type="radio" name="' . $question['question_id'] . '" value="' . $question['option_' . ($i + 1)] . '">
-            <label class="form-check-label" for="' . $question['question_id'] . '">
+            <input class="form-check-input ml-1" type="radio" name="' . $question['question_id'] . '" value="' . $question['option_' . ($i + 1)] . '">
+            <label class="form-check-label ml-4" for="' . $question['question_id'] . '">
                 ' . $question['option_' . ($i + 1)] . '
             </label>
     </div>';
                 }
                 echo '</fieldset>';
+                echo '</div>';
             } ?>
             <button type="submit" class="btn btn-primary col-4 m-3" id="fillForm">Send</button>
         </form>
     </div>
     <h5 class="float-right"><strong>Creator:</strong> <a href="<?php echo base_url().'profile/'.$userInfo[0]['user_id'] ?>"><?php echo $userInfo[0]['user_name'] ?></a></h5>
 </div>
+</page>
+
 
 <?php include dirname(__DIR__, 1) . '/sections/footer.php'; ?>
