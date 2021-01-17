1cdca0bfe94fad1dbe171b27a18c520380bd1e6f
Gökhan Göksel Elpeze <gokselelpeze@gmail.com>
2021-01-17 18:17:56 +0300

    anket calisiyor bozulmadan..

--- application/views/pages/questionnaire.php
+++ application/views/pages/questionnaire.php
@@ -9,32 +9,32 @@ else
 <div class="card mt-3">
     <div class="card-header bg-primary text-white font-weight-bolder bg-success"><p class="text-uppercase">Anket 1</p>
     </div>
-    <div class="card-body">
-        <ul class="list-group">
-            <li class="list-group-item">First item</li>
-            <li class="list-group-item">Second item</li>
-            <li class="list-group-item">Third item</li>
-        </ul>
-    </div>
-    <div class="card-footer d-flex justify-content-between">
-        <button type="button" class="btn btn-primary">Çöz</button>
-        <button type="button" class="btn btn-secondary">Rapor</button>
-    </div>
+
 </div>
 <div class="container mt-5">
-    <div class="col mt-4">
-        <div class="form-group col-4">
-            <input type="text" class="form-control" placeholder="Untitled Questionnaire" id="pollName">
+    <div class="d-flex">
+        <div class="form-group col-12">
+            <div class="form-group col-4">
+                <input type="text" class="form-control" placeholder="Untitled Questionnaire" id="qnName">
+            </div>
+            <div class="form-group col-4">
+                <input type="text" class="form-control" placeholder="Description" id="description">
+            </div>
+            <div class="form-group col-4">
+                <input type="text" class="form-control" placeholder="Type" id="qnType">
+            </div>
         </div>
-        <div class="row mt-4">
-            <button type="button" class="btn btn-primary col-4" id="addQuestion">Add Question</button>
-            <button type="button" class="btn btn-primary col-4" id="saveQuestion">Save</button>
-        </div>
+
 
 
     </div>
-    <div class="d-flex flex-column" id="polls">
 
+    <div class="d-flex flex-column" id="questions">
+
+    </div>
+    <div class="col mt-4 justify-content-between">
+        <button type="button" class="btn btn-primary col-4" id="addQuestion">Add Question</button>
+        <button type="button" class="btn btn-primary col-4" id="saveQuestion">Save</button>
     </div>
 </div
 
@@ -42,7 +42,7 @@ else
 <script>
     $(document).ready(function () {
         var $addQuestion = $('#addQuestion');
-        var $polls = $('#polls');
+        var $questions = $('#questions');
         var $savePool = $('#savePool');
 
         window.id = 1;
@@ -57,37 +57,53 @@ else
         });
 
         $addQuestion.on('click', function () {
-            var pollName = $('#pollName').val();
-            var pollType = $('#pollType').val();
-            pollItem(pollName, pollType, window.id);
+            var qnType = $('#qnType').val();
+            questionItem(qnType, window.id);
         })
 
         // ANKET EKLEME
-        function pollItem(pollName, pollType, id) {
-
-            var poll = $(`<div class="card mt-3" id="soru-${id}">
-                            <div class="card-header">${pollName}</div>
-                            <div class="card-body">
-                                ${pollType}
-                                <ul class="list-group">
-
-                                </ul>
-                            </div>
-                            <div class="card-footer d-flex justify-content-between row">
-                                <input type="text" class="form-control question" placeholder="Soru">
-                                <button type="button" class="btn btn-primary add-question">Soru Ekle</button>
+        function questionItem(qnType, id) {
+            var poll;
+            if (qnType === "Multiple Choice") {
+                poll = $(`<div class="card mt-3" id="soru-${id}">
+                            <div class="card-header">
+                    <input type="text" style="font-size:25px;" class="form-control" placeholder="Question" id="question">
+                       <ul class="list-group mt-2">
+                            <li class="list-group-item">
+                                <div contentEditable="true">
+                                    <span contentEditable="false">
+                                      <input type="radio"/>
+                                    </span>Option 1
+                                </div>
+                            </li>
+                      </ul>
+                            <div class="card-footer d-flex justify-content-between row mt-2">
+                                <input type="text" class="form-control question" placeholder="Option">
+                                <button type="button" class="btn btn-primary add-option mt-2">Add Option</button>
                             </div>
                         </div>`);
+            } else {
+                poll = $(`<div class="card mt-3" id="soru-${id}">
+                            <div class="card-header">
+                    <input type="text" style="font-size:25px;" class="form-control" placeholder="Question" id="question">
+                       <ul class="list-group mt-2">
+                             <div class="card-footer d-flex justify-content-between row mt-2">
+                                <input type="text" class="form-text" placeholder="Answer">
+                            </div>
+                      </ul>
+
+                        </div>`);
+            }
 
             // SORU EKLEME
-            poll.find('.add-question').on('click', function () {
-                var quesitionItem = $('<li class="list-group-item"><span class="quesiton-name"></span></li>').addClass('bg-success mt-2');
+            poll.find('.add-option').on('click', function () {
+                var quesitionItem = $('<li class="list-group-item"> <div contentEditable="true"> <span contentEditable="false"> <input type="radio"/> </span>Option 1 </div> </li>').addClass('mt-2');
                 var question = poll.find('.question').val();
                 quesitionItem.find('.quesiton-name').text(question);
                 poll.find('.list-group').append(quesitionItem);
             })
 
-            $polls.append(poll);
+            $questions.append(poll);
             window.id += 1;
         }
 
