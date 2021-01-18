d346dd1e3a687b49d2af6a63922c21276e9faf83
Gökhan Göksel Elpeze <gokselelpeze@gmail.com>
2021-01-19 00:59:11 +0300

    reformat

--- application/views/pages/about-us.php
+++ application/views/pages/about-us.php
@@ -4,64 +4,81 @@ if ($this->session->userdata('currently_logged_in'))
 else
     include dirname(__DIR__, 1) . '/sections/header.php';
 ?>
-<div class="container mt-5">
-    <div class="row py-3">
-        <div class="col-12 mt-5">
-            <h4>We are a team from Dokuz Eylül University Computer Engineering students.</h4><br>
-            <p>You can access our social media profiles from the links below.</p>
-        </div>
-        <div class="col-sm-4">
-            <div class="card">
-                <img class="card-img-top" src="<?php echo base_url()?>assets/img/ibrahim.jpg" alt="Card image" style="width:100%">
-                <div class="card-body">
-                    <h4 class="card-title">Halil İbrahim ÇAĞIRKAN</h4>
-                    <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
-                    <div class="d-flex justify-content-center">
-                    <ul class="social-network social-circle">
-                        <li><a href="#" target="_blank" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
-                        <li><a href="https://www.linkedin.com/in/çağırkan/" target="_blank" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
-                        <li><a href="#" target="_blank" class="icoInstagram" title="Instagram"><i class="fa fa-instagram"></i></a></li>
-                        <li><a href="#" target="_blank" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
-                    </ul>
+    <div class="container mt-5">
+        <div class="row py-3">
+            <div class="col-12 mt-5">
+                <h4>We are a team from Dokuz Eylül University Computer Engineering students.</h4><br>
+                <p>You can access our social media profiles from the links below.</p>
+            </div>
+            <div class="col-sm-4">
+                <div class="card">
+                    <img class="card-img-top" src="<?php echo base_url() ?>assets/img/ibrahim.jpg" alt="Card image"
+                         style="width:100%">
+                    <div class="card-body">
+                        <h4 class="card-title">Halil İbrahim ÇAĞIRKAN</h4>
+                        <p class="card-text">Some example text some example text. John Doe is an architect and
+                            engineer</p>
+                        <div class="d-flex justify-content-center">
+                            <ul class="social-network social-circle">
+                                <li><a href="#" target="_blank" class="icoFacebook" title="Facebook"><i
+                                                class="fa fa-facebook"></i></a></li>
+                                <li><a href="https://www.linkedin.com/in/çağırkan/" target="_blank" class="icoLinkedin"
+                                       title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+                                <li><a href="#" target="_blank" class="icoInstagram" title="Instagram"><i
+                                                class="fa fa-instagram"></i></a></li>
+                                <li><a href="#" target="_blank" class="icoTwitter" title="Twitter"><i
+                                                class="fa fa-twitter"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card">
+                    <img class="card-img-top" src="<?php echo base_url() ?>assets/img/ibrahim.jpg" alt="Card image"
+                         style="width:100%">
+                    <div class="card-body">
+                        <h4 class="card-title">Gökhan Göksel Elpeze</h4>
+                        <p class="card-text">Some example text some example text. John Doe is an architect and
+                            engineer</p>
+                        <div class="d-flex justify-content-center">
+                            <ul class="social-network social-circle">
+                                <li><a href="#" target="_blank" class="icoFacebook" title="Facebook"><i
+                                                class="fa fa-facebook"></i></a></li>
+                                <li><a href="https://www.linkedin.com/in/çağırkan/" target="_blank" class="icoLinkedin"
+                                       title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+                                <li><a href="#" target="_blank" class="icoInstagram" title="Instagram"><i
+                                                class="fa fa-instagram"></i></a></li>
+                                <li><a href="#" target="_blank" class="icoTwitter" title="Twitter"><i
+                                                class="fa fa-twitter"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card">
+                    <img class="card-img-top" src="<?php echo base_url() ?>assets/img/ibrahim.jpg" alt="Card image"
+                         style="width:100%">
+                    <div class="card-body">
+                        <h4 class="card-title">Berhan Türkü Ay</h4>
+                        <p class="card-text">Some example text some example text. John Doe is an architect and
+                            engineer</p>
+                        <div class="d-flex justify-content-center">
+                            <ul class="social-network social-circle">
+                                <li><a href="#" target="_blank" class="icoFacebook" title="Facebook"><i
+                                                class="fa fa-facebook"></i></a></li>
+                                <li><a href="https://www.linkedin.com/in/çağırkan/" target="_blank" class="icoLinkedin"
+                                       title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+                                <li><a href="#" target="_blank" class="icoInstagram" title="Instagram"><i
+                                                class="fa fa-instagram"></i></a></li>
+                                <li><a href="#" target="_blank" class="icoTwitter" title="Twitter"><i
+                                                class="fa fa-twitter"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="col-sm-4">
-            <div class="card">
-                <img class="card-img-top" src="<?php echo base_url()?>assets/img/ibrahim.jpg" alt="Card image" style="width:100%">
-                <div class="card-body">
-                    <h4 class="card-title">Gökhan Göksel Elpeze</h4>
-                    <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
-                    <div class="d-flex justify-content-center">
-                    <ul class="social-network social-circle">
-                        <li><a href="#" target="_blank" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
-                        <li><a href="https://www.linkedin.com/in/çağırkan/" target="_blank" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
-                        <li><a href="#" target="_blank" class="icoInstagram" title="Instagram"><i class="fa fa-instagram"></i></a></li>
-                        <li><a href="#" target="_blank" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
-                    </ul>
-                </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-4">
-            <div class="card">
-                <img class="card-img-top" src="<?php echo base_url()?>assets/img/ibrahim.jpg" alt="Card image" style="width:100%">
-                <div class="card-body">
-                    <h4 class="card-title">Berhan Türkü Ay</h4>
-                    <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
-                    <div class="d-flex justify-content-center">
-                    <ul class="social-network social-circle">
-                        <li><a href="#" target="_blank" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
-                        <li><a href="https://www.linkedin.com/in/çağırkan/" target="_blank" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
-                        <li><a href="#" target="_blank" class="icoInstagram" title="Instagram"><i class="fa fa-instagram"></i></a></li>
-                        <li><a href="#" target="_blank" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
-                    </ul>
-                </div>
-                </div>
-            </div>
-        </div>
-
     </div>
-</div>
-<?php include dirname(__DIR__, 1) . '/sections/footer.php';?>
+<?php include dirname(__DIR__, 1) . '/sections/footer.php'; ?>
--- application/views/pages/fill.php
+++ application/views/pages/fill.php
@@ -10,41 +10,44 @@ $userInfo = json_decode(json_encode($user), true);
 <page size="A4">
     <nav class="d-flex justify-content-lg-center">
         <div>
-            <a class="mr-5" href="<?php echo base_url();?>fill/<?php echo $qnInfo[0]['questionnaire_id'] ?>">Questions</a>
+            <a class="mr-5"
+               href="<?php echo base_url(); ?>fill/<?php echo $qnInfo[0]['questionnaire_id'] ?>">Questions</a>
         </div>
         <div>
-            <a href="<?php echo base_url();?>responses/<?php echo $qnInfo[0]['questionnaire_id']?>">Responses</a>
+            <a href="<?php echo base_url(); ?>responses/<?php echo $qnInfo[0]['questionnaire_id'] ?>">Responses</a>
         </div>
     </nav>
-<div class="container py-5">
-    <h1 class="text-center"><?php echo $qnInfo[0]['questionnaire_name'] ?></h1>
-    <h2 class="text-secondary text-center"><?php echo $qnInfo[0]['questionnaire_subtext'] ?></h2>
+    <div class="container py-5">
+        <h1 class="text-center"><?php echo $qnInfo[0]['questionnaire_name'] ?></h1>
+        <h2 class="text-secondary text-center"><?php echo $qnInfo[0]['questionnaire_subtext'] ?></h2>
 
-    <div class="ml-5 ">
-        <form>
-            <?php foreach ($questionsInfo as $question) {
-                echo '<div class="question mb-5">';
-                echo '<fieldset id="' . $question['question_id'] . '">';
-                echo '<h2>' . $question['question_name'] . '</h2>';
-                echo '<h5 class="text-secondary">' . $question['question_subtext'] . '</h5>';
-                for ($i = 0; $i < 8; $i++) {
-                    if ($question['option_' . ($i + 1)] == null)
-                        break;
-                    echo '<div class="my-1">
+        <div class="ml-5 ">
+            <form>
+                <?php foreach ($questionsInfo as $question) {
+                    echo '<div class="question mb-5">';
+                    echo '<fieldset id="' . $question['question_id'] . '">';
+                    echo '<h2>' . $question['question_name'] . '</h2>';
+                    echo '<h5 class="text-secondary">' . $question['question_subtext'] . '</h5>';
+                    for ($i = 0; $i < 8; $i++) {
+                        if ($question['option_' . ($i + 1)] == null)
+                            break;
+                        echo '<div class="my-1">
             <input class="form-check-input ml-1" type="radio" name="' . $question['question_id'] . '" value="' . $question['option_' . ($i + 1)] . '">
             <label class="form-check-label ml-4" for="' . $question['question_id'] . '">
                 ' . $question['option_' . ($i + 1)] . '
             </label>
     </div>';
-                }
-                echo '</fieldset>';
-                echo '</div>';
-            } ?>
-            <button type="submit" class="btn btn-primary col-4 m-3" id="fillForm">Send</button>
-        </form>
+                    }
+                    echo '</fieldset>';
+                    echo '</div>';
+                } ?>
+                <button type="submit" class="btn btn-primary col-4 m-3" id="fillForm">Send</button>
+            </form>
+        </div>
+        <h5 class="float-right"><strong>Creator:</strong> <a
+                    href="<?php echo base_url() . 'profile/' . $userInfo[0]['user_id'] ?>"><?php echo $userInfo[0]['user_name'] ?></a>
+        </h5>
     </div>
-    <h5 class="float-right"><strong>Creator:</strong> <a href="<?php echo base_url().'profile/'.$userInfo[0]['user_id'] ?>"><?php echo $userInfo[0]['user_name'] ?></a></h5>
-</div>
 </page>
 
 
