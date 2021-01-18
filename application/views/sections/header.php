d68ca70a5610c9b3fba8024032938f476a80556f
Gökhan Göksel Elpeze <gokselelpeze@gmail.com>
2021-01-19 01:29:11 +0300

    home button sola gitti

--- application/views/sections/header.php
+++ application/views/sections/header.php
@@ -14,6 +14,14 @@
     </head>
     <body>
         <nav class="navbar navbar-expand-md navbar-dark fixed-top mb-5">
+            <div class="mx-auto order-0">
+                <a class="navbar-brand mx-auto" href="<?php echo base_url(); ?>">
+                    <img src="<?php echo base_url() ?>assets/img/deuqueLogo.png">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
             <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
@@ -26,14 +34,6 @@
                         <a class="nav-link" href="<?php echo base_url(); ?>contact">Contact</a>
                     </li>
                 </ul>
-            </div>
-            <div class="mx-auto order-0">
-                <a class="navbar-brand mx-auto" href="<?php echo base_url(); ?>">
-                    <img src="<?php echo base_url() ?>assets/img/deuqueLogo.png">
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
             </div>
             <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
                 <ul class="navbar-nav ml-auto">
