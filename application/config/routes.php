54ef15a3e94745a481af7c2ca0ce6474e8e9a637
Gökhan Göksel Elpeze <gokselelpeze@gmail.com>
2021-01-19 23:36:21 +0300

    final

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,7 +52,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'main';
 $route['login'] = 'login';
 $route['logout'] = 'login/logout';
-$route['answers'] = 'main/data/';
 $route['login-page'] = 'main/login/';
 $route['signin'] = 'login/signin/';
 $route['profile/(:num)'] = 'main/profile/$1';
--- application/controllers/main.php
+++ application/controllers/main.php
@@ -8,14 +8,6 @@ class main extends CI_Controller
         $this->load->view("pages/index.php");
     }
 
-    public function data()
-    {
-        $this->load->model("answers_model");
-        $data['answers'] = $this->answers_model->getAllData();
-
-        $this->load->view("pages/answers.php", $data);
-    }
-
     public function login()
     {
         $this->load->view("login/login-page.php");
