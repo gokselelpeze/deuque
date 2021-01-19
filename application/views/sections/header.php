e2d97856564b4909f1e4b8ebc0d6e1fd3f8ebb69
Gökhan Göksel Elpeze <gokselelpeze@gmail.com>
2021-01-19 19:49:15 +0300

    css

--- application/views/sections/header.php
+++ application/views/sections/header.php
@@ -5,7 +5,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/style/style.css">
+        <link rel="stylesheet" href="<?php echo base_url();?>assets/style/style.css">
+        <link rel="stylesheet" href="<?php echo base_url();?>assets/style/main.css">
         <link rel='icon' href='<?php echo base_url();?>/assets/img/deuqueLogo32.png' type='image/x-icon' sizes="32x32"/>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
