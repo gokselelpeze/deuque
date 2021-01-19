5c25032d0f40994bd39f4d462bab60ada5c00746
Gökhan Göksel Elpeze <gokselelpeze@gmail.com>
2021-01-19 03:14:54 +0300

    cevaplar geliyor piechartlara cizilebiliyor hangi şıkkın kaç kez işaretlendiği bilgisi lazım sadece.

--- application/views/pages/responses.php
+++ application/views/pages/responses.php
@@ -20,54 +20,64 @@ $userInfo = json_decode(json_encode($user), true);
     <div class="container mt-5 py-5">
         <h1 class="text-center"><?php echo $qnInfo[0]['questionnaire_name'] ?></h1>
         <h2 class="text-secondary text-center"><?php echo $qnInfo[0]['questionnaire_subtext'] ?></h2>
-        <div class="">
+<!--        <div class="">
             <ul class=" list-group p-2">
                 <?php
-                for ($i = 0; $i < 8; $i++) {
+/*                for ($i = 0; $i < 8; $i++) {
                     if ($questionsInfo[0]['option_' . ($i + 1)] != null) {
                         echo '<li class="d-flex justify-content-between list-group-item">
             <label for="option ' . $i . '">' . $questionsInfo[0]['option_' . ($i + 1)] . '</label>
                                 </li>';
                     }
                 }
+                */?>
+            </ul>-->
+
+                <?php
+                $count = 1;
+                foreach ($questionsInfo as $row){
+
                 ?>
-            </ul>
-            <div class="ml-5 mt-3 pieChart">
                 <script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.4/Chart.bundle.min.js'></script>
                 <script>
+
                     $(document).ready(function () {
                         <?php
                         $phpArray = array();
                         for ($i = 0; $i < 8; $i++) {
-                            if ($questionsInfo[0]['option_' . ($i + 1)] != null)
-                                $phpArray[$i] = $questionsInfo[0]['option_' . ($i + 1)];
+                            if ($row['option_' . ($i + 1)] != null)
+                                $phpArray[$i] = $row['option_' . ($i + 1)];
                         }
                         ?>
+                        window.id = 1;
                         var labelArr = <?php echo json_encode($phpArray); ?>;
-                        var ctx = $("#chart-line");
+                        console.log(labelArr);
+                        var ctx = $("#chart-line<?php echo $count?>");
                         var myLineChart = new Chart(ctx, {
                             type: 'pie',
                             data: {
                                 labels: labelArr,
                                 datasets: [{
-                                    data: [1200, 1700, 800],
+                                    data: [1200, 1700, 800,1200, 1700, 800,1200, 1700],
                                     backgroundColor: ["rgba(255, 0, 0, 0.5)", "rgba(100, 255, 0, 0.5)", "rgba(200, 50, 255, 0.5)"]
                                 }]
                             },
                             options: {
                                 title: {
                                     display: true,
-                                    text: "<?php echo $questionsInfo[0]['question_name']?>"
+                                    text: "<?php echo $row['question_name']?>"
                                 }
                             }
                         });
                     });
+
                 </script>
+
                 <div class="page-content page-container" id="page-content">
                     <div class="padding">
                         <div class="row">
                             <div class="container-fluid d-flex justify-content-center">
-                                <div class="col-sm-8 col-md-6">
+                                <div class="col-sm-8 col-md-6 pieChart">
                                     <div class="chartjs-size-monitor"
                                          style="position: absolute; left: 0px; top: 0px; right: 0px; bottom: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;">
                                         <div class="chartjs-size-monitor-expand"
@@ -79,7 +89,7 @@ $userInfo = json_decode(json_encode($user), true);
                                             <div style="position:absolute;width:200%;height:200%;left:0; top:0"></div>
                                         </div>
                                     </div>
-                                    <canvas id="chart-line" width="299" height="200" class="chartjs-render-monitor"
+                                    <canvas id="chart-line<?php echo $count?>" width="299" height="200" class="chartjs-render-monitor"
                                             style="display: block; width: 299px; height: 200px;"></canvas>
                                 </div>
                             </div>
@@ -88,6 +98,11 @@ $userInfo = json_decode(json_encode($user), true);
                 </div>
             </div>
         </div>
+
+    <?php
+    $count++;
+    }
+    ?>
         <h5 class="float-right"><strong>Creator:</strong> <a
                     href="<?php echo base_url() . 'profile/' . $userInfo[0]['user_id'] ?>"><?php echo $userInfo[0]['user_name'] ?></a>
         </h5>
